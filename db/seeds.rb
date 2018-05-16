bb0a4c68ff8a9607b016ddff2095923154b524e1
mg ayoub <mg.ayoub@lewagon.org>
2018-05-15 22:49:02 -0400

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ require "faker"
 10.times do
   article = Article.new(
     title: Faker::Book.title,
-    content: Faker::BackToTheFuture.quote
+    content: Faker::Lorem.sentence(10)
   )
   article.save!
 end
